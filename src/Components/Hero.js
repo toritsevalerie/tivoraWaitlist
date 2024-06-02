c7d885f4d91f75d6d27bb2841a4f004c2fb0511a
ToritseTuedor <torisevalerie@gmail.com>
2024-06-02 18:41:23 -0400

    changes made to form, form added, hero page in progress

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,4 +1,23 @@
+import { useState } from "react";
+
 const Hero = () => {
+  const [isFormVisible, setFormVisible] = useState(false);
+  const [isThankYouMessageVisible, setThankYouMessageVisible] = useState(false);
+
+  const handleWaitlistClick = () => {
+    setFormVisible(!isFormVisible);
+    setThankYouMessageVisible(false);
+  };
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // prevents refresh of page
+    setThankYouMessageVisible(!isThankYouMessageVisible);
+    setFormVisible(!isFormVisible);
+  };
+
   return (
     <div>
       <div>
@@ -7,10 +26,36 @@ const Hero = () => {
           Design, customize & order unique packaging & branding solutions that
           align with your brand’s identity — delivered to your doorstep.
         </p>
-        <button>
-          <div>Join Waitlist</div>
-          <img src="" alt="" />
-        </button>
+        <div>
+          <button onClick={handleWaitlistClick}>
+            <div>Join Waitlist</div>
+            <img src="" alt="" />
+          </button>
+          {isFormVisible && (
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <div>
+                <button type="submit">Done</button>
+              </div>
+            </form>
+          )}
+          {isThankYouMessageVisible && (
+            <div>
+              <p>Sent! Thank You :)</p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
